b261d6592050692cc0b227eeabca52bd60438389
Abu Bakar Nawaz <abnawaz14@gmail.com>
2023-10-11 02:25:51 +0400

    remove gson library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -118,7 +118,6 @@ dependencies {
     implementation("androidx.room:room-runtime:${Version.room}")
     implementation("androidx.room:room-ktx:${Version.room}")
     ksp("androidx.room:room-compiler:${Version.room}")
-    implementation("com.google.code.gson:gson:2.10.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/compose/app/navigation/nav_graph/repeated_routes/MainRoute.kt
+++ app/src/main/java/com/compose/app/navigation/nav_graph/repeated_routes/MainRoute.kt
@@ -2,11 +2,9 @@ package com.compose.app.navigation.nav_graph.repeated_routes
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import com.compose.app.data.firebase.model.UserModel
 import com.compose.app.data.firebase.model.fromJson
 import com.compose.app.navigation.nav_graph.main.MainDestination
 import com.compose.app.presentation.main.screen.MainScreen
-import com.google.gson.Gson
 
 fun NavGraphBuilder.mainRoute() {
     composable(
